610a49e9e87000ca0663440ca6296f920f6c6d53
Drew Hynes <drew.hynes@gmail.com>
2021-08-27 07:25:20 -0400

    cleaned up master

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'dword4'
+SITENAME = 'dword4.github.io'
+SITEURL = 'https://dword4.github.io/'
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+THEME = "tuxlite_tbs"
+TYPOGRIFY = True
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('DazedPro', 'http://www.dazedpro.com/'),
+         ('mikep', 'http://mpietruszka.com/'),)
+
+# Social widget
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
